a86a74722c6da2f3a98a861b221acfb3777c1a52
Yuan Tao <towyuan@outlook.com>
2016-04-29 15:46:58 +0800

    Import all ano-lm commands that supported now

--- examples/test.js
+++ examples/test.js
@@ -1,9 +1,5 @@
 "use strict";
 
-var Arduino = require('../lib/arduino');
-var arduino = new Arduino();
+var lm = require('../lib/lm');
 
-// arduino.select('teensy:avr:teensy31');
-arduino.select('Arduino_STM32:STM32F1:mapleMini');
-// arduino.select('arduino:avr:uno');
-console.log(arduino.getPlatformPreferences());
+console.log(lm.commands);
